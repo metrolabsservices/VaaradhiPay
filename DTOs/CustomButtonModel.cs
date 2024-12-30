618c2efc3b01219c161f6f404f3ffd33a81b4832
Chandu <chandhupepakayala@gmail.com>
2024-12-30 14:58:54 +0530

    changing INR price,adding payment custom buttons

--- DTOs/CustomButtonModel.cs
+++ DTOs/CustomButtonModel.cs
@@ -4,5 +4,8 @@
     {
         public string DisplayText { get; set; } = string.Empty; // Button text
         public string NavigationLink { get; set; } = string.Empty; // Navigation link
+        public string Icon { get; set; } = string.Empty; // Icon associated with the button
+        public bool IsButtonDisabled { get; set; } = false; // Flag to disable navigation link
+        public bool? IsButtonActive { get; set; }
     }
 }
